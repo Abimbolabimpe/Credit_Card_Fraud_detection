fefc431be1ae576c943d53cd15e02678415129d5
Abimbolabimpe <146237978+Abimbolabimpe@users.noreply.github.com>
2023-10-01 22:36:28 +0100

    Update fraud.py
    To make the app work.

--- deploy/fraud.py
+++ deploy/fraud.py
@@ -11,37 +11,40 @@ model = joblib.load('deploy/my_model_saved.pkl')
 # Define the user interface
 st.title('Fraud Credit Card Prediction')
 
-TransactionAmt = st.number_input('Transaction Amount')
-TransactionDT = st.number_input('Transaction Date and Time (no timestamp)', format='%.0f')
-card1 = st.number_input('Card1', format='%.0f')
+C4 = st.number_input('C4', format='%.0f')
 C14 = st.number_input('C14', format='%.0f')
 C13 = st.number_input('C13', format='%.0f')
-card2 = st.number_input('Card2', format='%.0f')
-C8 = st.number_input('C8', format='%.0f')
-addr1 = st.number_input('Addr1',format='%.0f')
-v257 = st.number_input('V257', format='%.0f')
-C1 = st.number_input('C1', format='%.0f')
+V294= st.number_input('V294', format='%.0f')
+D3 = st.number_input('D3', format='%.0f')
+TransactionAmt = st.number_input('Transaction Amount')
+V97 = st.number_input('V97', format='%.0f')
+V317 = st.number_input('V317', format='%.0f')
+V280 = st.number_input('V280', format='%.0f')
+V295 = st.number_input('V295',format='%.0f')
+
 
 # Define the predict function
-def predict(TransactionAmt, TransactionDT, card1, C14, C13, card2, C8, addr1, v257, C1):
+
+
+def predict(C4, C14, C13, V294, D3, TransactionAmt, V97, V317, V280, V295):
     X = pd.DataFrame({
-        'TransactionAmt': TransactionAmt,
-        'TransactionDT': TransactionDT,
-        'card1': card1,
+        'C4': C4,
         'C14': C14,
         'C13': C13,
-        'card2': card2,
-        'C8': C8,
-        'addr1': addr1,
-        'V257': v257,
-        'C1': C1
+        'V294': V294,
+        'D3': D3,
+        'TransactionAmt': TransactionAmt,
+        'V97': V97,
+        'V317': V317,
+        'V280': V280,
+        'V295': V295
     }, index=[0])
     y_pred = model.predict(X)[0]
     return y_pred
 
 # Add the Predict button
 if st.button('Predict'):
-    result = predict(TransactionAmt, TransactionDT, card1, C14, C13, card2, C8, addr1, v257, C1)
+    result = predict(C4, C14, C13, V294, D3, TransactionAmt, V97, V317, V280, V295)
     if result == 0:
         st.write("The transaction is not fraud.")
     else:
