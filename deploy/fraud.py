9f49bb9b5343c5976cac98169dfb69076088b6b4
Abimbolabimpe <146237978+Abimbolabimpe@users.noreply.github.com>
2023-10-02 11:28:04 +0100

    Update fraud.py

--- deploy/fraud.py
+++ deploy/fraud.py
@@ -11,40 +11,41 @@ model = joblib.load('deploy/my_model_saved.pkl')
 # Define the user interface
 st.title('Fraud Credit Card Prediction')
 
-C4 = st.number_input('C4', format='%.0f')
 C14 = st.number_input('C14', format='%.0f')
 C13 = st.number_input('C13', format='%.0f')
+C4 = st.number_input('C4', format='%.0f')
 V294= st.number_input('V294', format='%.0f')
-D3 = st.number_input('D3', format='%.0f')
+C8 = st.number_input('C8', format='%.0f')
 TransactionAmt = st.number_input('Transaction Amount')
-V97 = st.number_input('V97', format='%.0f')
+card6 = st.number_input('card6', format='%.0f')
 V317 = st.number_input('V317', format='%.0f')
 V280 = st.number_input('V280', format='%.0f')
-V295 = st.number_input('V295',format='%.0f')
+V97 = st.number_input('V97',format='%.0f')
 
 
 # Define the predict function
 
 
-def predict(C4, C14, C13, V294, D3, TransactionAmt, V97, V317, V280, V295):
+def predict(C14, C13, C4, V294, C8, TransactionAmt, card6, V317, V280, V97):
+
     X = pd.DataFrame({
-        'C4': C4,
         'C14': C14,
         'C13': C13,
+        'C4': C4,
         'V294': V294,
-        'D3': D3,
+        'C8': C8,
         'TransactionAmt': TransactionAmt,
-        'V97': V97,
+        'card6': card6,
         'V317': V317,
         'V280': V280,
-        'V295': V295
+        'V97': V97
     }, index=[0])
     y_pred = model.predict(X)[0]
     return y_pred
 
 # Add the Predict button
 if st.button('Predict'):
-    result = predict(C4, C14, C13, V294, D3, TransactionAmt, V97, V317, V280, V295)
+    result = predict(C14, C13, C4, V294, C8, TransactionAmt, card6, V317, V280, V97)
     if result == 0:
         st.write("The transaction is not fraud.")
     else:
